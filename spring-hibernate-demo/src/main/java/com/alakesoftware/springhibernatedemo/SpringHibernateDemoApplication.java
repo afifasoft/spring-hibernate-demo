875f58e542628ae3bf4729ad7366272d71ae9590
afifasoft <abdul.pesit@gmail.com>
2020-03-29 13:04:03 +0530

    first commit

---
+++ spring-hibernate-demo/src/main/java/com/alakesoftware/springhibernatedemo/SpringHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alakesoftware.springhibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateDemoApplication.class, args);
+	}
+
+}
---
+++ spring-hibernate-demo/src/test/java/com/alakesoftware/springhibernatedemo/SpringHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alakesoftware.springhibernatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
